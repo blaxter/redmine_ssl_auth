0185b20b799de7c854eb164b22ae784fe409d86a
Jesús García Sáez <blaxter@gmail.com>
2009-04-01 18:51:32 +0200

    register user on the fly

--- lib/redmine_ssl_auth/monkey_patches.rb
+++ lib/redmine_ssl_auth/monkey_patches.rb
@@ -3,19 +3,22 @@ module RedmineSslAuth
     module AccountPatch
       def login_with_ssl_auth
         if !User.current.logged? and not params[:skip_ssl] and try_ssl_auth
-          redirect_back_or_default :controller => 'my', :action => 'page'
+          if @registered_on_the_fly
+            redirect_to :controller => 'my', :action => 'account'
+          else
+            redirect_back_or_default :controller => 'my', :action => 'page'
+          end
           return
         end
-                
+
         login_without_ssl_auth
       end
-      
+
       module InstanceMethods
         def try_ssl_auth
           session[:email] = request.env["SSL_CLIENT_S_DN_CN"]
           if session[:email]
-            user = User.find_by_mail(session[:email])
-            # TODO: try to register on the fly
+            user = User.find_by_mail(session[:email]) || register_on_the_fly( session[:email] )
             unless user.nil?
               # Valid user
               return false if !user.active?
@@ -25,6 +28,22 @@ module RedmineSslAuth
             end
           end
           false
+        end
+
+        def register_on_the_fly( email )
+          username = email.split( '@' ).first
+          user = User.new( :mail      => email,
+                           :firstname => username,
+                           :lastname  => username,
+                           :language  => Setting.default_language )
+          user.admin = false
+          user.login = username
+          user.password = username
+          user.password_confirmation = username
+          if user.save
+            @registered_on_the_fly = true
+            Mailer.deliver_account_information( user, username )
+          end && user
         end
       end
       
